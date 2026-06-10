21f12394508e596c08d0eeb8ca30befdf81c6a74
Elite203 <solankihardik334@gmail.com>
2026-06-10 16:41:17 +0530

    Upload via Git Herder

---
+++ src/pages/Timesheets.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '@/components/layout/Layout';
+import { TimesheetsContent } from '@/components/timesheets/TimesheetsContent';
+
+export default function TimesheetsPage() {
+  return (
+    <Layout>
+      <TimesheetsContent />
+    </Layout>
+  );
+}
