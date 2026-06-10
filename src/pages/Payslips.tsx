01f7c3692d2c1d6a98fdc89f8fc9244e5ae47cc5
Elite203 <solankihardik334@gmail.com>
2026-06-10 16:41:08 +0530

    Upload via Git Herder

---
+++ src/pages/Payslips.tsx
@@ -0,0 +1,10 @@
+import { PayslipDashboard } from "@/components/payslips/PayslipDashboard";
+import { Layout } from "@/components/layout/Layout";
+
+export default function Payslips() {
+  return (
+    <Layout>
+      <PayslipDashboard />
+    </Layout>
+  );
+}
