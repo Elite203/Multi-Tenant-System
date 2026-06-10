0bfa0d67cc9b5ef7284abcc554317ee99872d201
Elite203 <solankihardik334@gmail.com>
2026-06-10 16:40:54 +0530

    Upload via Git Herder

---
+++ src/pages/Documents.tsx
@@ -0,0 +1,10 @@
+import { Layout } from "@/components/layout/Layout";
+import { DocumentsContent } from "@/components/documents/DocumentsContent";
+
+export default function Documents() {
+  return (
+    <Layout>
+      <DocumentsContent />
+    </Layout>
+  );
+}
