61487432b96133285b708fc6eb25a9d259c0f698
Elite203 <solankihardik334@gmail.com>
2026-06-10 16:40:52 +0530

    Upload via Git Herder

---
+++ src/pages/DataImport.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "@/components/layout/Layout";
+import { DataImportContent } from "@/components/data-import/DataImportContent";
+
+const DataImportPage = () => {
+  return (
+    <Layout>
+      <DataImportContent />
+    </Layout>
+  );
+};
+
+export default DataImportPage;
